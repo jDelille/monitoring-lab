0921834070024ea8d92876e30702012e96d03574
Justin Delille <justind7513@gmail.com>
2021-09-28 14:08:02 -0700

    second

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 app.use(express.json())
 
+app.use(express.static("client"));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
