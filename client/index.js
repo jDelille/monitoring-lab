7ac425d068fe0dcab34fbea16be3f2728c578a31
Justin Delille <justind7513@gmail.com>
2021-09-28 13:57:36 -0700

    initial commit

---
+++ client/index.js
@@ -0,0 +1,16 @@
+gsap.from(".btn", {
+    // rotation: 360,
+    x: -500,
+    duration: 1,
+    opacity: 0
+});
+
+
+
+let btn = document.querySelector('.btn')
+btn.addEventListener('click', () => {
+    axios.get('/api/compliments')
+        .then((req, res) => {
+            console.log(res.data)
+        })
+})
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/index.html'))
+})
+
+
+//ARRAY STUFF - - - - - - - - 
+const niceThings = ['you are cool', 'you are a savage', 'you are a beast']
+
+app.get('/api/compliments', (req, res) => {
+   let compliments =  Math.floor(Math.random() * niceThings.length)
+   console.log(compliments)
+   res.status(200).send(niceThings[compliments]);
+})
+
+//heroku port 
+const PORT = process.env.PORT || 4000
+
+// app.use(rollbar.errorHandler())
+
+app.listen(PORT, () => console.log(`Avengers... Assemble on port: ${PORT}`));
