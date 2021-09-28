0a962187a9fa4285e7369ff05579c6f9e4342797
Justin Delille <justind7513@gmail.com>
2021-09-28 14:03:00 -0700

    second

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ app.use(express.json())
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/index.html'))
+    res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
 
