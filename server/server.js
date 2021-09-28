8f4e84e2e631e27a2299f5d7ea73699c14c0656a
Justin Delille <justind7513@gmail.com>
2021-09-28 14:38:10 -0700

    updated style and key

--- server/server.js
+++ server/server.js
@@ -6,6 +6,14 @@ app.use(express.json())
 
 app.use(express.static("client"));
 
+//ROLLBAR - - - - - - - - 
+const Rollbar = require('rollbar');
+const rollbar = new Rollbar({
+    accessToken: '466fd71534f043ca8b9e8d9d6898602f',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
@@ -18,11 +26,12 @@ app.get('/api/compliments', (req, res) => {
    let compliments =  Math.floor(Math.random() * niceThings.length)
    console.log(compliments)
    res.status(200).send(niceThings[compliments]);
+   rollbar.log('Student successfully added!')
 })
 
 //heroku port 
 const PORT = process.env.PORT || 4000
 
-// app.use(rollbar.errorHandler())
+app.use(rollbar.errorHandler())
 
 app.listen(PORT, () => console.log(`Avengers... Assemble on port: ${PORT}`));
